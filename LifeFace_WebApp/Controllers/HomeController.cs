7fbaffd7c9e35834b06c3c5392e4fb38b2f486b9
VinhHuynh <huynhquangvinh01121999@gmail.com>
2021-08-11 11:27:33 +0700

    master
    create client

---
+++ LifeFace_WebApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using LifeFace_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LifeFace_WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
